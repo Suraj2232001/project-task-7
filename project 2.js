19e7118f37eeac6d0e5e1383055126d7aef96cbe
Suraj2232001 <67924985+Suraj2232001@users.noreply.github.com>
2020-07-08 22:36:47 +0530

    project task 7 javascript

---
+++ project 2.js
@@ -0,0 +1,18 @@
+var myAge = 20;
+//age is assigned to myAge variable
+var earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//new values is assigned to earlyYears variable
+console.log(`the earlyage is ${earlyYears} years`);
+//print the statement of earlyAge
+var laterYears = (myAge - 2) * 4;
+//using arithmetic operator and saving the value to laterYears variable
+console.log(`the laterage is ${laterYears} years`);
+//print the statement of laterAge
+var MyAgeInDogYears = laterYears + earlyYears;
+//addition laterYears and earlyYears id saved to MyAgeInDogYears
+var string = "SURAJ";
+myName = string.toLowerCase();
+//the toLowerCase is a method that returns the string to lowercase characters
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${MyAgeInDogYears} years old in dog years.`)
+//print the statement 
